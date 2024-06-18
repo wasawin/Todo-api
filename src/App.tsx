440c5c0c3541e62d797728c2d23f2087964da8f1
wasawin <wasawin.14@gmail.com>
2024-06-18 23:26:40 +0700

    not do any

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl font-bold underline">
+        Todo
+      </h1>
+      <div className="bg-red-500 h-40 w-0">
+        asda
+      </div>
+
+    </main>
+
   )
 }
