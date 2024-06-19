461bbb58c47ce17b518d74f547fb36cba6981cde
wasawin <wasawin.14@gmail.com>
2024-06-20 00:13:24 +0700

    add function search

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 
-
+import React, { useState } from 'react'
 
 
 export default function App() {
+  const [input, setInput] = useState('')
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if (!input.trim()) {
+      return
+    }
+    setInput('');
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-3">
       <h1 className="text-3xl font-bold underline ">
         Todo
       </h1>
-      <div className="bg-red-500 w-[70%] min-h-[500px] rounded-xl space-y-5 flex flex-col items-center justify-center">
+      <div className="bg-red-500 w-[100%] min-h-[500px] rounded-xl space-y-5 flex flex-col items-center justify-center">
 
-        <form action="" className="flex w-[70%]">
-          <input type="text" className="bg-slate-200 p-2 grow" placeholder="Search your todo" />
+        <form action="" className="flex w-[70%]" onSubmit={handleSubmit}>
+          <input type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="bg-slate-200 p-2 grow" placeholder="Search your todo" />
 
-          <button type="submit" className="bg-blue-300 p-2 ">find</button>
+          <button
+            type="submit"
+            className="bg-blue-300 p-2 ">
+            find</button>
         </form>
 
 
