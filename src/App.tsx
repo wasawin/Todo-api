91b31a698c976daacd20d4cf6e3dcd51fe95b87c
wasawin <wasawin.14@gmail.com>
2024-06-21 20:54:22 +0700

    fix type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import React, { useEffect, useState } from 'react'
-import { todoType } from './type/todoType';
 import Todoitem from './components/Todoitem';
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -38,27 +37,33 @@ export default function App() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center p-24 gap-3">
-        <h1 className="text-3xl font-bold underline ">
+      <main className="h-screen  p-[4%] ">
+        <h1 className="text-3xl font-bold underline text-center ">
           Todo
         </h1>
-        <div className="bg-red-500 w-[100%] min-h-[500px] rounded-xl space-y-5 flex flex-col items-center justify-center">
+        <div className="bg-red-500  rounded-xl space-y-5 max-w-lg mx-auto ">
 
-          <form action="" className="flex w-[70%]" onSubmit={handleSubmit}>
+          <form action=""
+            className="flex justify-center p-3"
+            onSubmit={handleSubmit}>
+
             <input type="text"
               value={input}
               onChange={(e) => setInput(e.target.value)}
-              className="bg-slate-200 p-2 grow" placeholder="Search your todo" />
+              placeholder="Search your todo"
+              className="bg-slate-200 rounded-s-md grow text-center"
+            />
 
             <button
               type="submit"
-              className="bg-blue-300 p-2 ">
+              className="bg-blue-300 p-3 rounded-e-md hover:bg-black hover:text-white">
               find</button>
           </form>
 
           <div className="bg-slate-300 grow w-[70%]">
-            {todos.map((todos: todoType) =>
-              <Todoitem todo={todos} />
+            {todos.map((todos) =>
+
+              < Todoitem todo={todos} />
             )
 
             }
--- src/components/Todoitem.tsx
+++ src/components/Todoitem.tsx
@@ -1,13 +1,17 @@
 import { todoType } from '../type/todoType'
 
-export default function Todoitem({ todo }: todoType) {
+interface todoItemProps {
+    todo: todoType
+}
+
+
+export default function Todoitem({ todo }: todoItemProps) {
     return (
         <div>
             <div className='flex items-center justify-between'>
                 <div className='flex' >
                     <div className=''>{todo.id}</div>
                     <div className=' bg-slate-500'>{todo.title}</div>
-
                 </div>
             </div>
         </div>
